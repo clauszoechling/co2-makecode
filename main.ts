2df8bfe0620354673f058e73ba33b91b86880626
clauszoechling <42353424+clauszoechling@users.noreply.github.com>
2021-03-04 14:15:24 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -119,10 +119,8 @@ namespace CO2 {
     //% block
     //% shim=sendBufferAsm
     //% group="LEDs"
-    export function sendBuffer(led1: string, led2: string, led3: string, led4: string, pin: DigitalPin) {
+    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
     }
-    /*export function sendBuffer(buf: Buffer, pin: DigitalPin) {
-    }*/
 
     //% shim=setBufferMode
     //% pin.defl=DigitalPin.P12
