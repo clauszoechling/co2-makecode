e3d3e27df91b35aa5d63e8093b551bb4aff59e02
clauszoechling <42353424+clauszoechling@users.noreply.github.com>
2021-03-05 12:02:49 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,4 @@
-enum NeoPixelColors {
-    //% block=red
-    Red = 0xFF0000,
-    //% block=orange
-    Orange = 0xFFA500,
-    //% block=yellow
-    Yellow = 0xFFFF00,
-    //% block=green
-    Green = 0x00FF00,
-    //% block=blue
-    Blue = 0x0000FF,
-    //% block=indigo
-    Indigo = 0x4b0082,
-    //% block=violet
-    Violet = 0x8a2be2,
-    //% block=purple
-    Purple = 0xFF00FF,
-    //% block=white
-    White = 0xFFFFFF,
-    //% block=black
-    Black = 0x000000
-}
+
 
 /**
  * CO2 block
