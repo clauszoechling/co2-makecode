a2456c8db13e5efc0127ee21515e54ca36d08480
clauszoechling <42353424+clauszoechling@users.noreply.github.com>
2021-03-05 09:14:19 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -134,7 +134,7 @@ namespace CO2 {
             CO2.sendBuffer(hex`000000 000000 000000 000000 000000 000000 000000`, DigitalPin.P12)
             let buf3 = Buffer.fromUTF8("hex `ff0000 00ff00 0000ff ff0000 00ff00 0000ff ff0000`")
             //let temp = Buffer.fromHex("0xffff00ffffff0000ff")
-            CO2.sendBuffer(buf3, DigitalPin.P12)
+            ////CO2.sendBuffer(buf3, DigitalPin.P12)
             basic.pause(5000)
             //CO2.sendBuffer("hex + ` + list + ` ", DigitalPin.P12)
             //CO2.sendBuffer(hex farb_code3, DigitalPin.P12)
