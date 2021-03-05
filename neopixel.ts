222de3c97d64691c2bfdd862d1a6f1e93945a65c
clauszoechling <42353424+clauszoechling@users.noreply.github.com>
2021-03-05 12:18:31 +0100

    Update neopixel.ts

--- neopixel.ts
+++ neopixel.ts
@@ -40,7 +40,7 @@ enum NeoPixelMode {
  * Functions to operate NeoPixel strips.
  */
 //% weight=5 color=#2699BF icon="\uf110"
-namespace neopixel {
+namespace CO2 {
     /**
      * A NeoPixel strip
      */
