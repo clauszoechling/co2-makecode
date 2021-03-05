33cbca34bd6f4675abdcdcfa6f93a139ddce1d7a
clauszoechling <42353424+clauszoechling@users.noreply.github.com>
2021-03-05 10:05:10 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -137,7 +137,7 @@ namespace CO2 {
             let var1 = "hex "
             let var2 = "000000 000000 000000 000000 000000 000000 000000"
             let gesamt = var1 + var2
-            let versuch = Buffer.fromUTF8("gesamt")
+            let versuch = Buffer.fromUTF8("255")
             CO2.sendBuffer(versuch, DigitalPin.P12)
             basic.pause(3000)
  
