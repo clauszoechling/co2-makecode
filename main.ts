5c26d8888e18902a6e2553a0dd577e614d940786
clauszoechling <42353424+clauszoechling@users.noreply.github.com>
2021-03-06 17:17:32 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -130,13 +130,13 @@ namespace CO2 {
         CO2_Werte_zeitlich.insertAt(4, co2)
         
     //kopiere
-    for (let Index = 0; Index <= 4; Index++) {
+    /*for (let Index = 0; Index <= 4; Index++) {
             CO2_Werte_sortiert.insertAt(Index, CO2_Werte_zeitlich[Index])
-        }
+        }*/
 
 
     //sortiere
-    for (let k = 0; k <= 4; k++) {
+    /*for (let k = 0; k <= 4; k++) {
         for (let i = 0; i <= 3 - k; i++) {
             if (CO2_Werte_sortiert[i] > CO2_Werte_sortiert[i + 1]) {
                 temp = CO2_Werte_sortiert[i]
@@ -144,9 +144,9 @@ namespace CO2 {
                 CO2_Werte_sortiert[i + 1] = temp
             }
         }
-    }
+    }*/
 
-    return CO2_Werte_sortiert[2]
+    //return CO2_Werte_sortiert[2]
 
     }
 
