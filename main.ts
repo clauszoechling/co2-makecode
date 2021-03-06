d2d7aeee718881282c0b2fdd4ff659a95de9e36d
clauszoechling <42353424+clauszoechling@users.noreply.github.com>
2021-03-06 16:12:59 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
-
+let CO2_Werte_zeitlich = [400,400,400,400,400]
 
 /**
  * CO2 block
  */
 //%color=#FF8409 icon="\uf185" block="CO2"
 //% groups="['CO2', ,'LEDs', 'WIFI', 'Display']"
-
-let CO2_Werte_zeitlich = [400,400,400,400,400]
 
 namespace CO2 {
     
