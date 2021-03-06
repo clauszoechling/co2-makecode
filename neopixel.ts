5f96f63250cf438c5af119c0b2af71c7efbdfdd1
clauszoechling <42353424+clauszoechling@users.noreply.github.com>
2021-03-06 11:18:14 +0100

    Aktualisiere neopixel.ts

--- neopixel.ts
+++ neopixel.ts
@@ -539,7 +539,7 @@ namespace CO2 {
      * Gets the RGB value of a known color
     */
     //% weight=2 blockGap=8
-    //% blockId="neopixel_colors" block="%color"
+    
     //% subcategory="LEDs"
     //% group="LEDs"
     
